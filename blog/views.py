462b0771994bdb5e02a3d4b3ffec4a199513e002
Vivek Shetye <vivekshetye@Viveks-MacBook-Air.local>
2019-02-07 20:57:59 -0500

    updated view templates

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+
+posts = [
+    {
+        'author' : 'Vivek Shetye',
+        'title' : 'Blog Post 1',
+        'content' : 'First blog post content',
+        'date' : 'Feb 6 2019'
+    },
+    {
+        'author' : 'Vivek Shetye',
+        'title' : 'Blog Post 2',
+        'content' : 'Second blog post content',
+        'date' : 'Feb 6 2019'
+    }
+]
 
 
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts' : posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title' : 'About'})
