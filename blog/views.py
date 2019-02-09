999550635ea7495ac90dd1b97228e3417dcdaef1
Vivek Shetye <vivekshetye@Viveks-MacBook-Air.local>
2019-02-08 21:22:53 -0500

    added sqllite database to backend and created db of Posts

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,10 @@
 from django.shortcuts import render
-
-
-posts = [
-    {
-        'author' : 'Vivek Shetye',
-        'title' : 'Blog Post 1',
-        'content' : 'First blog post content',
-        'date' : 'Feb 6 2019'
-    },
-    {
-        'author' : 'Vivek Shetye',
-        'title' : 'Blog Post 2',
-        'content' : 'Second blog post content',
-        'date' : 'Feb 6 2019'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts' : posts
+        'posts' : Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
