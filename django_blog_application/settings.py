9504fcfb4124fd41fce63bfe147dfe79d2cdfe32
Vivek Shetye <vivekshetye@Viveks-MacBook-Air.local>
2019-02-16 17:46:10 -0500

    updated database to mysql and added other post pages

--- django_blog_application/settings.py
+++ django_blog_application/settings.py
@@ -77,10 +77,18 @@ WSGI_APPLICATION = 'django_blog_application.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'Hyperparameter',
+    'HOST': '127.0.0.1',
+    'PORT': '3306',
+    'USER': 'root',
+    'PASSWORD': 'vivek',
+}
 }
 
 
